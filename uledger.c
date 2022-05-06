7411e177236cdc17a2b2ebb96343d803d72e0d57
Hyunseo Lee <hslee@handong.ac.kr>
2022-05-06 10:21:45 +0900

    Create uledger.c & update header

---
+++ uledger.c
@@ -0,0 +1,45 @@
+#include "uledger.h"
+
+int mainMenu() {
+
+}
+
+int addData(Entry *p[], int count) {
+
+}
+
+void printData(Entry *p[], int count) {
+
+}
+
+void printByCategory(Entry *p[], int count, int category) {
+
+}
+
+void printByDate(Entry *p[], int count, int date[]) {
+
+}
+
+void updateData(Entry *p[]) {
+
+}
+
+int deleteData(Entry *p[], int count) {
+
+}
+
+void searchData(Entry *p[], int count) {
+
+}
+
+void analyzeData(Entry *p[], int count) {
+
+}
+
+int loadData(Entry *p[]) {
+
+}
+
+void saveData(Entry *p[], int count) {
+
+}
